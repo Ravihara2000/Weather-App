4b5172551f83cc5287c17ad79520722061681ee8
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-05-06 09:39:00 +0530

    working on search bar

--- Home.js
+++ Home.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, StyleSheet, ImageBackground } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  Text,
+  TextInput,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { deviceHeight, deviceWidth } from "./Dimensions";
 
@@ -10,11 +17,15 @@ export default function Home() {
         source={require("./assets/bgpic1.jpg")}
         style={{ width: deviceWidth, height: deviceHeight }}
       />
-      <View style={{ position: "absolute" }}>
+      <View
+        style={{
+          position: "absolute",
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+        }}
+      >
         <View
           style={{
-            paddingVertical: 20,
-            paddingHorizontal: 10,
             flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
@@ -22,6 +33,34 @@ export default function Home() {
           }}
         >
           <Icon name="menu" size={46} color="white" />
+          <Image
+            source={require("./assets/user.jpg")}
+            style={{ marginTop: 26, height: 46, width: 46, borderRadius: 50 }}
+          />
+        </View>
+
+        <View style={{ paddingHorizontal: 20, marginTop: 100 }}>
+          <Text style={{ fontSize: 40, color: "white" }}>Hellow Ravihara</Text>
+          <Text style={{ color: "white", fontSize: 22, fontWeight: "bold" }}>
+            Serach the city by the name
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderRadius: 20,
+              borderEndWidth: 1,
+              borderColor: "white",
+            }}
+          >
+            <TextInput
+              placeholder="Serach the city"
+              placeholderTextColor="white"
+              style={{ paddingHorizontal: 10, color: "white" }}
+            />
+            <Icon name="search" size={22} color="white" />
+          </View>
         </View>
       </View>
     </View>
